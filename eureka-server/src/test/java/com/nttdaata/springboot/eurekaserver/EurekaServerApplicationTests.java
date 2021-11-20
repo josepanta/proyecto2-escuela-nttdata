455241cfb96224f28718a80a346f2b07ec048ded
josepanta <josepanta63@gmail.com>
2021-11-20 16:39:02 -0500

    add module

---
+++ eureka-server/src/test/java/com/nttdaata/springboot/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdaata.springboot.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
