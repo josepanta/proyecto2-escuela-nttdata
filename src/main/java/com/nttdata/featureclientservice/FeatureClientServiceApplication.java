21ee049efbe7c9a5a235f3292622fbe9ecdb1794
Rodrigo Canaza <alonzo_5sep@hotmail.com>
2021-11-21 01:07:32 -0500

    Init

---
+++ src/main/java/com/nttdata/featureclientservice/FeatureClientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.featureclientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureClientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureClientServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/featureclientservice/FeatureClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.featureclientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
