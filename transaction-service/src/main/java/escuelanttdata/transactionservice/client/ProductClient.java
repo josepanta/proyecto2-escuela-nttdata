f002bd0883a04eaa6ff74c20eab51dd13f3ce9eb
Luis Ronaldo Ventura Ordaz <64562477+Ronaldo-Ventura@users.noreply.github.com>
2021-11-21 23:31:29 -0500

    fix2 transaction-service

--- transaction-service/src/main/java/escuelanttdata/transactionservice/client/ProductClient.java
+++ transaction-service/src/main/java/escuelanttdata/transactionservice/client/ProductClient.java
@@ -3,8 +3,8 @@ package escuelanttdata.transactionservice.client;
 import escuelanttdata.transactionservice.client.model.Product;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 
 @FeignClient(name = "service-product")
@@ -13,6 +13,6 @@ public interface ProductClient {
     @GetMapping("/product/{id}")
     Product getById(@PathVariable Integer id);
 
-    @PatchMapping("/product")
+    @PutMapping("/product")
     void updateProduct(@PathVariable Product product);
 }
